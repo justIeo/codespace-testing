5b6f4a1dec48e898641c9c1940589ef70d765e2d
leo <somebody1337666@gmail.com>
2022-05-24 19:07:01 +0000

    more files!!!

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+const fs = require('node:fs');
+const path = require('node:path');
 require('dotenv').config()
 const discord = require('discord.js')
-const client = new discord.Client({intents: [discord.Intents.FLAGS.GUILD_MEMBERS]})
+const client = new discord.Client({intents: [discord.Intents.FLAGS.GUILDS]})
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'cmds');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag} (${client.user.id})`)
+})
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
